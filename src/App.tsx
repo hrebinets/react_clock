96b4436988b2e94578d610054d90130d0da51bdc
Max_Hrebinets <brainisbro@gmail.com>
2024-12-11 19:16:55 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.scss';
 
 function getRandomName(): string {
@@ -8,30 +8,82 @@ function getRandomName(): string {
 }
 
 export const App: React.FC = () => {
-  const today = new Date();
-  let clockName = 'Clock-0';
+  const [hasClock, setHasClock] = useState(true);
+  const [clockName, setClockName] = useState('Clock-0');
+  const [today, setToday] = useState(new Date().toUTCString().slice(-12, -4));
 
-  // This code starts a timer
-  const timerId = window.setInterval(() => {
-    clockName = getRandomName();
-  }, 3300);
+  // #region Event Handlers
+  const hideClock = (event: MouseEvent) => {
+    event.preventDefault();
+    setHasClock(false);
+  };
 
-  // this code stops the timer
-  window.clearInterval(timerId);
+  const showClock = () => {
+    setHasClock(true);
+  };
+
+  useEffect(() => {
+    document.addEventListener('contextmenu', hideClock);
+    document.addEventListener('click', showClock);
+
+    return () => {
+      document.removeEventListener('contextmenu', hideClock);
+      document.removeEventListener('click', showClock);
+    };
+  }, []);
+  // #endregion
+
+  useEffect(() => {
+    setToday(new Date().toUTCString().slice(-12, -4));
+    let timerId: number | null = null;
+    let timePerSecond: number | null = null;
+
+    if (hasClock) {
+      timerId = window.setInterval(() => {
+        setClockName(prevName => {
+          const newName = getRandomName();
+
+          // eslint-disable-next-line no-console
+          console.warn(`Renamed from ${prevName} to ${newName}`);
+
+          return newName;
+        });
+      }, 3300);
+
+      timePerSecond = window.setInterval(() => {
+        const currentTime = new Date().toUTCString().slice(-12, -4);
+
+        setToday(currentTime);
+
+        // eslint-disable-next-line no-console
+        console.log(currentTime);
+      }, 1000);
+
+      return () => {
+        if (timerId !== null) {
+          window.clearInterval(timerId);
+        }
+
+        if (timePerSecond !== null) {
+          window.clearInterval(timePerSecond);
+        }
+      };
+    }
+  }, [hasClock]);
 
   return (
     <div className="App">
       <h1>React clock</h1>
 
-      <div className="Clock">
-        <strong className="Clock__name">{clockName}</strong>
+      {hasClock && (
+        <div className="Clock">
+          <strong className="Clock__name">{clockName}</strong>
 
-        {' time is '}
+          {' time is '}
 
-        <span className="Clock__time">
-          {today.toUTCString().slice(-12, -4)}
-        </span>
-      </div>
+          <span className="Clock__time">{today}</span>
+        </div>
+      )}
     </div>
   );
 };
